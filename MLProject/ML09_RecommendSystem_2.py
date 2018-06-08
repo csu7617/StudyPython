e44b6bbc4e8eb484fd54f099eca6d5508332454f
csu7617 <iamamagnet@outlook.com>
2018-06-08 16:52:32 +0800

    完整写完三个推荐系统：依据用户相似度，m*n，三元组

--- MLProject/ML09_RecommendSystem_2.py
+++ MLProject/ML09_RecommendSystem_2.py
@@ -1,9 +1,8 @@
 '''
-Created on 2018年6月4日
+Created on Jun 7, 2018
 
-@author: HW
+@author: hw
 '''
- 
 import numpy as np
 import tensorflow as tf
 from builtins import len
@@ -61,17 +60,19 @@ def data_process(data):
             moive_index = np.argwhere(user_data[:,1]==j+1).reshape(-1)
             if moive_index.shape[0]>0:
                 use_data[i,j] = float(user_data[moive_index[0],2])-u_movie[j]-u_user[i]
-    print(np.array(use_data).shape)
+#     print(use_data)
     return u_user,u_movie,use_data
 
 def training(training_number,use_data,filter_predict,feature_size,learning_rate = 0.01,lamda = 0.01,min_loss = 0.01):
+    
+   
     starttime = datetime.datetime.now()
     use_data = np.array(use_data)
     use_data = use_data.reshape([use_data.shape[0],-1])
-    
     x = tf.Variable(tf.truncated_normal([use_data.shape[0],feature_size],stddev=0.02))
-    
+#     x = tf.Variable(tf.random_uniform([use_data.shape[0],feature_size],-1/np.sqrt(feature_size),1/np.sqrt(feature_size)))
     w = tf.Variable(tf.truncated_normal([use_data.shape[1],feature_size],stddev=0.02))
+#     w = tf.Variable(tf.random_uniform([use_data.shape[1],feature_size],-1/np.sqrt(feature_size),1/np.sqrt(feature_size)))
     predict = tf.matmul(x,tf.transpose(w))
     predict = tf.multiply(filter_predict, predict)
     loss = tf.reduce_sum(tf.square(predict-use_data))+lamda*tf.reduce_sum(tf.square(x)) + lamda*tf.reduce_sum(tf.square(w))
@@ -82,10 +83,13 @@ def training(training_number,use_data,filter_predict,feature_size,learning_rate 
         sess.run(init)
         for train_step in range(training_number):
             sess.run(train)
+            if train_step %200 == 0:
+                print(sess.run(loss))
 #             if(sess.run(loss)<=min_loss):
 #                 break
+
         saver = tf.train.Saver()
-        saver.save(sess, 'data/model/model_ml09_rs_2.ckpt')
+        saver.save(sess, 'data/model/model_ml09_rs_1.ckpt')
 #             print(sess.run(loss))
     tf.reset_default_graph()   
     endtime = datetime.datetime.now()
@@ -99,8 +103,9 @@ def test(use_data,filter_predict,feature_size,u_user,u_movie,which_user,recommen
     use_data = use_data.reshape([use_data.shape[0],-1])
     
     x = tf.Variable(tf.truncated_normal([use_data.shape[0],feature_size],stddev=0.02))
-    
+#     x = tf.Variable(tf.random_uniform([use_data.shape[0],feature_size],-1/np.sqrt(feature_size),1/np.sqrt(feature_size)))
     w = tf.Variable(tf.truncated_normal([use_data.shape[1],feature_size],stddev=0.02))
+#     w = tf.Variable(tf.random_uniform([use_data.shape[1],feature_size],-1/np.sqrt(feature_size),1/np.sqrt(feature_size)))
     predict = tf.matmul(x,tf.transpose(w))
     predict = tf.multiply(filter_predict, predict)
     
@@ -108,29 +113,45 @@ def test(use_data,filter_predict,feature_size,u_user,u_movie,which_user,recommen
         init = tf.global_variables_initializer()
         sess.run(init)
         saver = tf.train.Saver()
-        saver.restore(sess, 'data/model/model_ml09_rs_2.ckpt')
+        saver.restore(sess, 'data/model/model_ml09_rs_1.ckpt')
         x_ = sess.run(x)
         w_ = sess.run(w)
     x = x_
     w = w_
     tf.reset_default_graph()
     
+    #找到用户对电影所有的评分
     x_user = x[which_user-1,:]
+    
+    #找到未预测电影的index
     filter_predict_user = filter_predict[which_user-1,:]
     unrating_index = np.argwhere(filter_predict_user==0).reshape(-1)
-    print(np.array(w[unrating_index,:]).shape,np.array(x_user).shape)
+    
+    #进行预测
     predict = np.dot(w[unrating_index,:],np.transpose(x_user)).reshape(-1)
     
-#     print(predict)
     
     predict = predict + u_user[which_user-1]+ u_movie[unrating_index]
     predict = predict[0:137]
     recommend = [] 
+    max = 5
     for i in range(recommend_num):
+        #找到预测中的最大值
         max_index = np.argmax(predict)
-        recommend.append({unrating_index[max_index]+1:predict[max_index]})
+#         if i == 0:
+#             max = predict[max_index]
+        recommend.append({unrating_index[max_index]+1:(predict[max_index]/max) *5})
+        #在预测值以及index中删除最大值
         predict = np.delete(predict,max_index)
         unrating_index = np.delete(unrating_index,max_index)
+        if(unrating_index.shape[0]<=0):
+            break
+        
+    for i in range(recommend_num):
+        min_index = np.argmin(predict)
+        recommend.append({unrating_index[min_index]+1:predict[min_index]})
+        predict = np.delete(predict,min_index)
+        unrating_index = np.delete(unrating_index,min_index)
         if(unrating_index.shape[0]<=0):
             break
     return recommend
@@ -138,7 +159,7 @@ def test(use_data,filter_predict,feature_size,u_user,u_movie,which_user,recommen
 
 if __name__=='__main__':
     
-    training_number = 100000
+    training_number = 10000
     data_filepath = 'data/ml-100k/u1.base'
     split_flag = '\t'
     encoding = 'utf-8'
@@ -148,10 +169,7 @@ if __name__=='__main__':
     filter_predict = np.array([[0.0 if use_data[i,j]==0 else 1.0 for j in range(use_data.shape[1])] for i in range(use_data.shape[0])])#当use_data中的元素为0时，则这个矩阵相应位置为0，其余位置为1
     filter_predict =filter_predict.astype(np.float32)
     print(filter_predict.shape)
-    training(training_number,use_data,filter_predict,feature_size=5,learning_rate = 0.05,lamda = 0.01,min_loss = 0.01)
+    training(training_number,use_data,filter_predict,feature_size=5,learning_rate = 0.05,lamda = 0.1,min_loss = 0.1)
     
     recommend = test(use_data,filter_predict,5,u_user,u_movie,1,5)
     print(recommend)
-    
-
-    
--- MLProject/Test.py
+++ MLProject/Test.py
@@ -11,7 +11,13 @@ from sklearn import datasets
 import random
 import pandas as pd
 
-path = "data/iris_test.csv"
-#names添加列名，header用指定的行为作为标题，若无原标题则设为None
-data = pd.read_csv(path,header=None,names= ['SepalLength','SepalWidth','PetalLength','PetalWidth','Species'])
-print(data)
+# path = "data/iris_test.csv"
+# #names添加列名，header用指定的行为作为标题，若无原标题则设为None
+# data = pd.read_csv(path,header=None,names= ['SepalLength','SepalWidth','PetalLength','PetalWidth','Species'])
+# print(data)
+
+
+a = [[1,2,3],[4,5,6],[7,8,9]]
+a = np.array(a)
+a = np.sum(a,axis = 1) 
+print(a)
